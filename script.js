8b419f390fd0937046724b5c73515cca06ece9eb
Xuan-ioe <3432677411@qq.com>
2025-11-25 00:26:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ $(document).ready(function() {
     // 排序下拉框变化事件
     $('#sortSelect').on('change', function() {
         const sortValue = $(this).val();
-        alert(`已按${$(this).find('option:selected').text()}重新排列作品`);
+        alert(`已${$(this).find('option:selected').text()}重新排列作品`);
         // 这里可以添加实际排序逻辑
     });
     
